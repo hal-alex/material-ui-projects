930025042b86c3ca4a406e732f87c683c304a1a6
Alex <1995aha@gmail.com>
2023-01-31 01:45:51 +0000

    testing hard wrap

--- src/Test.js
+++ src/Test.js
@@ -1,12 +1,33 @@
 import React from 'react'
-import { Typography } from '@mui/material'
+import { Typography, Button, TextField } from '@mui/material'
+// import Button from '@mui/material/Button'
 
 const Test = () => {
     return (
         <div>
-            <Typography>
+            <Typography variant="h1"
+                sx={{
+                    color: "red",
+                    border: "1px solid black",
+
+                }}
+            >
                 Hello
             </Typography>
+            <Button
+                onClick={() => alert("Button clicked")}
+                size='large' variant="contained" color='primary' >
+                Testing
+            </Button>
+            <Button size='large' variant="outlined" color='success'>
+                Testing
+            </Button>
+            <Button size='large' variant="text">
+                Testing
+            </Button>
+            <TextField type={"text"} label="Name" placeholder='Name' variant='outlined'></TextField>
+            <TextField type={"email"} label="email" placeholder='email' variant='filled'></TextField>
+            <TextField type={"password"} label="Name" placeholder='Name' variant='standard'></TextField>
         </div>
     )
 }
