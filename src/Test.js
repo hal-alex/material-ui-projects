fb1bf00da28e0fd2f9c4502218a5f5cb9d35a440
Alex <1995aha@gmail.com>
2023-01-31 00:49:52 +0000

    new project

---
+++ src/Test.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Typography } from '@mui/material'
+const Test = () => {
+    return (
+        <div>
+            <Typography>
+                Hello
+            </Typography>
+        </div>
+    )
+}
+
+export default Test
